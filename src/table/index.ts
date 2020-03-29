f1b38c8983d1a783be901164c7567275d73c147c
Jonathan Lee Slew <jonathan.leeslew@hotmail.com>
2020-03-29 14:58:05 +0400

    Updated demo

--- src/table/index.ts
+++ src/table/index.ts
@@ -4,6 +4,9 @@ declare const __webpack_public_path__;
 declare const TableStyle;
 declare const _;
 
+declare const async;
+declare const m;
+
 export default class TableComponent extends Component {
     public static getDependencies() {
         return Component.getDependencies().concat([
@@ -27,17 +30,33 @@ export default class TableComponent extends Component {
                     }),
                 ),
             ),
-            new Component(`tbody.${TableStyle[`tbody`]}`).add(
-                _.map(attrs.data() || [], function (row) {
-                    return new Component(`tr.${TableStyle[`tr`]}`).add(
-                        _.map(attrs.cols || [], function (col) {
-                            return new Component(`td.${TableStyle[`td`]}`).add(
-                                typeof col.render === `function` ? col.render(row) : row[col.name],
-                            ).set(`data-label`, _.startCase(_.lowerCase(col.name)));
-                        }),
-                    );
-                }),
-            ),
+            new Component(`tbody.${TableStyle[`tbody`]}`).add(function () {
+                var selected = attrs.selected || m.stream(attrs.selectable === `multiple` ? [] : ``);
+                var children = m.stream([]);
+
+                (function (components) {
+                    async.mapSeries(attrs.data() || [], function (row, callback) {
+                        new Component(`tr.${TableStyle[`tr`]}`).add(
+                            _.map(attrs.cols || [], function (col) {
+                                return new Component(`td.${TableStyle[`td`]}`).add(
+                                    typeof col.render === `function` ? col.render(row) : row[col.name],
+                                ).set(`data-label`, _.startCase(_.lowerCase(col.name)));
+                            }),
+                        ).decorate([
+                            attrs.strippable ? `${__webpack_public_path__}strippable.js` : ``,
+                            attrs.selectable ? `${__webpack_public_path__}selectable.js` : ``,
+                            attrs.hoverable ? `${__webpack_public_path__}hoverable.js` : ``,
+                        ], {
+                            selectable: attrs.selectable,
+                            value: JSON.stringify(row),
+                            components: components,
+                            selected: selected,
+                        }, callback);
+                    }).then(children);
+                })(m.stream([]));
+
+                return children;
+            }),
         ]);
     }
 };
