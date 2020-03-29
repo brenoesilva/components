737c56d7d4a92fae9ff111eb661ad54667e39660
Jonathan Lee Slew <jonathan.leeslew@hotmail.com>
2020-03-29 14:57:47 +0400

    Updated demo

--- src/index/demo.js
+++ src/index/demo.js
@@ -1,4 +1,9 @@
+require(`construct-style-sheets-polyfill`);
 require(`systemjs/dist/system.js`);
+
+System.import(`${__webpack_public_path__}index.css`).then(function (callback) {
+    document.adoptedStyleSheets = document.adoptedStyleSheets.concat(callback.default);
+});
 
 System.import(`${__webpack_public_path__}index.js`).then(function ($) {
     $.ready(function () {
