8a1e01fc4ca0e797cc48181e2e4c4f68f20fcc9c
Jonathan Lee Slew <jonathan.leeslew@hotmail.com>
2020-03-29 14:57:55 +0400

    Added selectable module

--- src/list/index.ts
+++ src/list/index.ts
@@ -1,7 +1,11 @@
 import Component from '../component/index.ts';
 
 declare const __webpack_public_path__;
+declare const styles;
 declare const _;
+
+declare const async;
+declare const m;
 
 export default class ListComponent extends Component {
     public static getDependencies() {
@@ -15,9 +19,29 @@ export default class ListComponent extends Component {
     }
 
     protected init(self, attrs) {
-        self.add(_.map(attrs.data(), function (item) {
-            var component = typeof attrs.render === `function` ? attrs.render(item) : item;
-            return new Component(`li`).add(component);
-        }));
+        var selected = attrs.selected || m.stream(attrs.selectable === `multiple` ? [] : ``);
+
+        self.add(function () {
+            var components = m.stream([]);
+
+            async.mapSeries(attrs.data(), function (item, callback) {
+                var component = function () {
+                    return function () {
+                        return typeof attrs.render === `function` ? attrs.render(item) : item;
+                    };
+                };
+
+                new Component(`li`).add(component).decorate([
+                    attrs.selectable ? `${__webpack_public_path__}selectable.js` : ``,
+                ], {
+                    selectable: attrs.selectable,
+                    components: components,
+                    selected: selected,
+                    value: item,
+                }, callback);
+            }).then(attrs.data);
+
+            return attrs.data;
+        });
     }
 };
